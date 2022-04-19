ac40eb9e4f4502c00b305ef54d8ba8c90090dc64
Илья <illia.a6ram@gmail.com>
2022-04-19 11:45:06 +0300

    HW

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -6,7 +6,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.opera.OperaDriver;
 import org.testng.ITestContext;
 import org.testng.annotations.*;
@@ -15,8 +14,6 @@ import pages.LoginPage;
 import pages.ProductsPage;
 
 import java.time.Duration;
-
-
 @Listeners(TestListener.class)
 public class BaseTest {
 
@@ -30,29 +27,20 @@ public class BaseTest {
     @Parameters({"browser"})
     @BeforeMethod
     public void setup(@Optional("chrome") String browser, ITestContext testContext) {
-        if(System.getProperty("browser","chrome").equalsIgnoreCase("chrome")){
-            if (browser.equalsIgnoreCase("chrome")) {
-                WebDriverManager.chromedriver().setup();
-                ChromeOptions options = new ChromeOptions();
-                if( System.getProperty("headless","true").equals("true"))
-                    options.addArguments("--headless");
-                driver = new ChromeDriver(options);
-            } else if (browser.equalsIgnoreCase("EDGE")) {
-                WebDriverManager.edgedriver().setup();
-                driver = new EdgeDriver();
-                driver.manage().window().maximize();
-            } else if (browser.equalsIgnoreCase("Opera")) {
-                WebDriverManager.operadriver().setup();
-                driver = new OperaDriver();
-                driver.manage().window().maximize();
-            }else if (browser.equalsIgnoreCase("FireFox")){
-                WebDriverManager.firefoxdriver().setup();
-                driver=new FirefoxDriver();
-                driver.manage().window().maximize();}
-            else  if (System.getProperty("browser","chrome").equalsIgnoreCase("firefox")){
-                WebDriverManager.firefoxdriver().setup();
-                driver=new FirefoxDriver();
-            }
+        if (browser.equalsIgnoreCase("chrome")) {
+            WebDriverManager.chromedriver().setup();
+            ChromeOptions options = new ChromeOptions();
+            if( System.getProperty("headless","true").equals("true"))
+                options.addArguments("--headless");
+            driver = new ChromeDriver(options);
+        } else if (browser.equalsIgnoreCase("EDGE")) {
+            WebDriverManager.edgedriver().setup();
+            driver = new EdgeDriver();
+            driver.manage().window().maximize();
+        } else if (browser.equalsIgnoreCase("Opera")) {
+            WebDriverManager.operadriver().setup();
+            driver = new OperaDriver();
+            driver.manage().window().maximize();
         }
         testContext.setAttribute("driver", driver);
         driver.manage().window().maximize();
